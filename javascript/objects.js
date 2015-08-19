b910fbfaa459e01002a47a93608d8ebaba7a8040
Mehdi Mehrtash <mehdi@Mehdis-MacBook-Pro.local>
2015-08-19 15:58:19 -0700

    add new object.js

---
+++ javascript/objects.js
@@ -0,0 +1,29 @@
+// Objects are like Hash in Ruby. Key's are always strings
+
+// var myObject = {
+//   a: 1,
+//   b: 2,
+//   c: 3
+// };
+//
+// You can access it with the "."
+// ex; myObject.a or myObject["a"]
+//
+// to add myObject["d"] = 4
+
+
+// var me = {name: "Mehdi", age: "35", occupation: "Student"};
+// console.log(me);
+//
+// me.occupation = "javascript Expert"
+// console.log(me);
+//
+// me.skill = ['ruby', 'rails', 'javascript'];
+// console.log(me);
+
+// var object = {b: [1,2,3], c: {d: 5}};
+//
+// object.b --> 1,2,3
+// object.b[1] --> 2
+// object.c --> {d: 5}
+// object.c.d --> 5
